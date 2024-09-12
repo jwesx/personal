618b992cba06aa84581427d1a6db2e07c4439eba
Jesus <mapmaking49@gmail.com>
2024-09-12 14:57:29 -0300

    invert camera and cmd

--- python/camera_conv/camera_ascii.py
+++ python/camera_conv/camera_ascii.py
@@ -8,10 +8,10 @@ import time
 muito_norm = [
     '@', '#', 'S', '%', '&', '$', 'B', '8', 'W', 'M', 'O', 'Q', 'G', 'D', 'A', 'E', 'F', 'H', 'K', 'P', 'R', 'T', 'Y', 'U', 'V', 'Z',
     'X', 'I', 'L', 'N', 'C', 'J', '0', '1', '2', '3', '4', '5', '6', '7', '9', '>', '<', '=', '?', '!', '+', '*', ':', ';', '^', '(', ')',
-    '[', ']', '{', '}', '|', '\\', '/', '_', '-', '`', '"', "'", ',', '.', '~', ' ', ' '
+    '[', ']', '{', '}', '|', '\\', '/', '_', '-', '`', '"', "'", ',', '.', '~', ' '
 ]
 muito_inv = [
-    ' ', ' ', '~', '.', ',', "'", '"', '`', '-', '_', '/', '\\', '|', '{', '}', '[', ']', ')', '(', '^', ';', ':', '*', '+', '!', '?', '=', '<', '>',
+    ' ', '~', '.', ',', "'", '"', '`', '-', '_', '/', '\\', '|', '{', '}', '[', ']', ')', '(', '^', ';', ':', '*', '+', '!', '?', '=', '<', '>',
     '9', '7', '6', '5', '4', '3', '2', '1', '0', 'J', 'C', 'N', 'L', 'I', 'X', 'Z', 'V', 'U', 'Y', 'T', 'R', 'P', 'K', 'H', 'F', 'E', 'A', 'D', 'G',
     'Q', 'O', 'M', 'W', '8', 'B', '$', '&', '%', 'S', '#', '@'
 ]
@@ -35,11 +35,12 @@ small_inv = [
 ASCII_CHARS = [muito, small_norm, small_inv]
 
 ascii_util = 0
+mirror_bool = True
 
-def cycle():
+def cycle_ascii():
     global ascii_util
     key = cv2.waitKey(1)
-        
+
     if key == 119:  # 'g'
         ascii_util += 1
         ascii_util %= len(ASCII_CHARS)
@@ -47,12 +48,21 @@ def cycle():
         ascii_util -= 1
         ascii_util %= len(ASCII_CHARS)
     return ascii_util
+   
+def cycle_mirror():
+    global mirror_bool
+    key = cv2.waitKey(1)
+    
+    if key == 97: #should be 'a'
+        mirror_bool = not mirror_bool
+    
+    return mirror_bool
 
 def nothing(x):
     pass
 
-def gray_to_ascii(image, new_width):
-    ascii_util = cycle()
+def gray_to_ascii(image, new_width, mirrored):
+    ascii_util = cycle_ascii()
     divisor = 255 // (len(ASCII_CHARS[ascii_util]) - 1)
 
     (original_width, original_height) = image.shape
@@ -61,18 +71,33 @@ def gray_to_ascii(image, new_width):
     resized_image = cv2.resize(image, (new_width, new_height))
 
     ascii_str = ""
-    for row in resized_image:
-        for pixel in row:
-            index = math.floor(pixel / divisor)
-            if index >= len(ASCII_CHARS[ascii_util]):
-                index = len(ASCII_CHARS[ascii_util]) - 1
-            ascii_str += ASCII_CHARS[ascii_util][index] # relativo ao ascii_chars
-        ascii_str += "\n"
+    if mirrored:
+        for row in resized_image:
+            line_str = "" 
+            for pixel in row:
+                index = math.floor(pixel / divisor)
+                if index >= len(ASCII_CHARS[ascii_util]):
+                    index = len(ASCII_CHARS[ascii_util]) - 1
+                line_str = ASCII_CHARS[ascii_util][index] + line_str
+            ascii_str += line_str + "\n"
+    else:
+        for row in resized_image:
+            for pixel in row:
+                index = math.floor(pixel / divisor)
+                if index >= len(ASCII_CHARS[ascii_util]):
+                    index = len(ASCII_CHARS[ascii_util]) - 1
+                ascii_str += ASCII_CHARS[ascii_util][index]
+            ascii_str += "\n"
+
     ascii_str += f"\n{ascii_util}"
+    
     return ascii_str
 
 def main():
     i = 0
+
+    prev_frame = 0
+    curr_frame = 0
 
     #define a camera acessada (0 é padrão do dispositivo)
     cam = cv2.VideoCapture(0)
@@ -87,26 +112,41 @@ def main():
         if not check:
             break
 
+        curr_frame = time.time()
+        fps = 1/(curr_frame - prev_frame)
+        prev_frame = curr_frame
+
+        fps = int(fps)
+        fps = str(fps)
+
         width = cv2.getTrackbarPos('widsld', 'Slider')
 
         if width <= 16:
             width = 200
+        
+        mirror_bool = cycle_mirror()
 
         #seta um fram cinza,
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2GRAY)
         #usa o cinza pra rodar transformar em ascii (facilitando -> deixa em apenas uma reta 0 à 255)
-        ascii_art = gray_to_ascii(gray_frame, width) # 16 -> 200; 5 -> 640
+        ascii_art = gray_to_ascii(gray_frame, width, mirror_bool) # 16 -> 200; 5 -> 640
 
-        #mostra o original e o cinza, por motivos de debug
-        cv2.imshow('Original', frame)
-        cv2.imshow('Cinza', gray_frame)
         cv2.resizeWindow('Slider', (100, 0))
 
-        #e, no cmd, o ascii
-        print(ascii_art)
-        i += 1
+        if not mirror_bool:
+            #mostra o original e o cinza, por motivos de debug
+            cv2.imshow('Original', frame)
+            cv2.imshow('Cinza', gray_frame)
+        else:
+            cv2.imshow('Original', cv2.flip(frame, 1))
+            cv2.imshow('Cinza', cv2.flip(gray_frame, 1))
         
-        if (i >= 50):
+
+        #e, no cmd, o ascii
+        print((ascii_art) + f'\n{fps}')
+        i += 1
+
+        if (i >= 25):
             os.system('cls')
             i = 0
 
